c8a1731a02db4de9b44b070b93f5d3e3788f366f
lcl <fmgag2021@gmail.com>
2022-11-11 15:21:47 +0800

    待解決:如何把經過時間放入laptime變數裡

--- stopwatch.dart
+++ stopwatch.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 void main(List<String> args) {
   real_time();
   //stopwatch();
@@ -20,19 +22,18 @@ class stopwatch {
   int now() => DateTime.now().millisecondsSinceEpoch;
 
   //開始
-  int start() {
+  int start_btn() {
     startAt == 0 ? startAt : now(); //感覺怪怪的，如果我main的startAt不動，永遠都是從0秒開始
-    return startAt;
   }
 
   //暫停
-  int stop() {
+  int stop_btn() {
     lapTime == startAt ? lapTime + now() - startAt : lapTime;
     startAt = 0;
   }
 
   //清除
-  int reset() => startAt = lapTime = 0;
+  int reset_btn() => startAt = lapTime = 0;
   //經過的時間
   int passTime() => lapTime + (startAt == now() ? now() - startAt : 0);
 }
@@ -42,8 +43,23 @@ var x = new stopwatch();
 //顯示經過時間
 class show {
   int update() => x.passTime();
+  int start() {
+    /*
+    //感覺可以計算秒數增加的程式碼，但現在問題卡在什麼時候暫停並叫stopwatch.stop_btn()
+    var counter = 3;
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      print(timer.tick);
+      counter--;
+      if (counter == 0) {
+        print('Cancel timer');
+        timer.cancel();
+      }
+    });
+    */
+  }
 }
-
+//當執行stopwatch.start()=> startAt隨著Timer.periodic持續更新數字//要測Timer.periodic可否被暫存
+//當執行 stopwatch.stop()時 => Timer.periodic cancel，並將其數字暫存到lapTime
 
 
 //顯示每個時間段做的事情
